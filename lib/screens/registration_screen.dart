d36d7ee2495a4c699db40e1178bd793ba198fa24
yossaj <yossaj@icloud.com>
2020-10-25 13:21:54 +0000

    timer page layout changed to match designs - countdown function added

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -3,8 +3,51 @@ import 'package:flutter/material.dart';
 class RegistrationScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Registration Screen'),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          RegTextField(),
+          RegTextField(),
+          RegTextField(),
+          FlatButton(
+            child: Text('Next'),
+            onPressed: (){
+              Navigator.pushNamed(context, '3');
+            },
+          )
+        ],
+      ),
     );
   }
 }
+
+class RegTextField extends StatefulWidget {
+  @override
+  _RegTextFieldState createState() => _RegTextFieldState();
+}
+
+class _RegTextFieldState extends State<RegTextField> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 20),
+      child: Container(
+        padding: EdgeInsets.only(bottom: 20, left: 8, right: 8),
+        child: TextField(
+          style: TextStyle(
+            fontSize: 20
+          ),
+        ),
+        decoration: BoxDecoration(
+          color: Color(0x44F16C6C),
+          borderRadius: BorderRadius.circular(5),
+
+        ),
+
+      ),
+    );
+  }
+}
+
--- lib/screens/time_screen.dart
+++ lib/screens/time_screen.dart
@@ -1,17 +1,73 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-class TimeScreen extends StatelessWidget {
+class TimeScreen extends StatefulWidget {
+
+
+  @override
+  _TimeScreenState createState() => _TimeScreenState();
+}
+
+class _TimeScreenState extends State<TimeScreen> {
+
+  Duration countDown;
+  String countDownText = '00:00:00';
+
+  @override
+  void initState() {
+    super.initState();
+    setCountdown();
+  }
+
+  void setCountdown(){
+    countDown = Duration(seconds: 40);
+
+      var timer = Timer.periodic(Duration(seconds: 1), (timer) {
+        if(countDown.inSeconds > 0) {
+          countDown -= Duration(seconds: 1);
+          setState(() {
+            countDownText = _formatDuration(countDown);
+          });
+        }else{
+          timer.cancel();
+        }
+      });
+
+  }
+
+  String _formatDuration(Duration duration) {
+    String twoDigits(int n) => n.toString().padLeft(2, "0");
+    String twoDigitMinutes = twoDigits(duration.inMinutes.remainder(60));
+    String twoDigitSeconds = twoDigits(duration.inSeconds.remainder(60));
+    return "${twoDigits(duration.inHours)}:$twoDigitMinutes:$twoDigitSeconds";
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.red,
+      backgroundColor: Colors.white,
       body: Center(
         child: Container(
-          child: Text(
-            'TimeScreen',
-          style: TextStyle(
-            color: Colors.white
-          ),),
+          height: 300,
+          width: 300,
+          decoration: BoxDecoration(
+            color: Colors.red,
+            borderRadius: BorderRadius.circular(150),
+            boxShadow: [BoxShadow(
+                color: Colors.red.shade100,
+                spreadRadius: 8,
+                blurRadius: 4,
+            )]
+          ),
+          child: Center(
+            child: Text(
+              countDownText,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 58
+            ),),
+          ),
         ),
       ),
     );
