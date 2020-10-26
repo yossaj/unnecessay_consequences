d268913e4957ec98e6240d29eb4894c904e9cf37
yossaj <yossaj@icloud.com>
2020-10-26 12:58:51 +0000

    adding twitter api elements

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,6 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:twitter_api/twitter_api.dart';
 
-class RegistrationScreen extends StatelessWidget {
+class RegistrationScreen extends StatefulWidget {
+
+
+  @override
+  _RegistrationScreenState createState() => _RegistrationScreenState();
+}
+
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+
+  final _twitterOauth = new twitterApi(
+      consumerKey: '',
+      consumerSecret: '',
+      token: '',
+      tokenSecret: ''
+  );
+
+  void triggerRequest() async{
+    Future twitterRequest = _twitterOauth.getTwitterRequest(
+    // Http Method
+    "POST",
+    // Endpoint you are trying to reach
+    "statuses/update.json",
+    // The options for the request
+    options: {
+    "status": "Testing for an app"// Used to prevent truncating tweets
+    },
+    );
+
+
+// Wait for the future to finish
+    var res = await twitterRequest;
+
+// Print off the response
+    print(res.statusCode);
+    print(res.body);
+  }
+
+
+  @override
+  void initState() {
+    triggerRequest();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/screens/time_screen.dart
+++ lib/screens/time_screen.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 class TimeScreen extends StatefulWidget {
 
